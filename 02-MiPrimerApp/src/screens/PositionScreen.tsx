4621ee5a9825324e5fe42d0e15c256047b0cdc26
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-06-02 11:54:20 +0200

    position

---
+++ 02-MiPrimerApp/src/screens/PositionScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet } from 'react-native';
+
+export const PositionScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#dc5050'
+    }
+});
