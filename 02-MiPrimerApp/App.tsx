56145c8a3e8ae47b60084f876386632f3d18856c
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-06-02 14:49:05 +0200

    flex

--- 02-MiPrimerApp/App.tsx
+++ 02-MiPrimerApp/App.tsx
@@ -4,6 +4,7 @@ import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 import {ContadorScreen} from './src/screens/ContadorScreen';
 import { DimensionesScreen } from './src/screens/DimensionesScreen';
+import { FlexScreen } from './src/screens/FlexScreen';
 import { PositionScreen } from './src/screens/PositionScreen';
 
 const App = () => {
@@ -13,7 +14,8 @@ const App = () => {
       {/* <ContadorScreen/> */}
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionesScreen/> */}
-      <PositionScreen/>
+      {/* <PositionScreen/> */}
+      <FlexScreen/>
     </SafeAreaView>
   );
 };
