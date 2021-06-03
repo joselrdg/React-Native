d4f4039b91866d9f8c4a56961b81cac8727c4801
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-06-03 23:23:50 +0200

    navigation stack

--- 04-navegacionApp/App.tsx
+++ 04-navegacionApp/App.tsx
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {StackNavigator} from './src/navigator/StackNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Text>Hola</Text>
+      <StackNavigator />
     </NavigationContainer>
   );
 };
