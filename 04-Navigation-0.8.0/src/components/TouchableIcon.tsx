6bffaae20043f34cd49fba4e075434692293dfc6
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-06-05 20:51:54 +0200

    Icons

---
+++ 04-Navigation-0.8.0/src/components/TouchableIcon.tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {TouchableOpacity} from 'react-native';
+import { colores } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
+
+interface Props {
+    iconName: string
+}
+
+export const TouchableIcon = ({iconName}: Props) => {
+
+  const {changeFavoriteIcon} = useContext(AuthContext)
+  return (
+    <TouchableOpacity
+    onPress={() => changeFavoriteIcon(iconName)}
+    >
+      <Icon name={iconName} size={80} color={colores.primary} />
+    </TouchableOpacity>
+  );
+};
--- 04-Navigation-0.8.0/src/screens/ContactsScreen.tsx
+++ 04-Navigation-0.8.0/src/screens/ContactsScreen.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, {useContext} from 'react';
+import {Button, Text, View} from 'react-native';
+import {AuthContext} from '../context/AuthContext';
+import {styles} from '../theme/appTheme';
 
 export const ContactsScreen = () => {
-    return (
-        <View>
-            <Text>ContactsScreen</Text>
-        </View>
-    )
-}
+  const {signIn, authState: {isLoggedIn}} = useContext(AuthContext);
+
+  return (
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>ContactsScreen</Text>
+      {!isLoggedIn && <Button title="SignIn" onPress={signIn} />}
+    </View>
+  );
+};
