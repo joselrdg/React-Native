539ec2f97815d565cc9b24969a522fb460077046
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-06-05 21:56:04 +0200

    Icons

--- 04-Navigation-0.9.0/App.tsx
+++ 04-Navigation-0.9.0/App.tsx
@@ -1,12 +1,13 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 // import { StackNavigator } from './src/navigator/StackNavigator';
 // import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
-import {MenuLateral} from './src/navigator/MenuLateral';
-import {AuthProvider} from './src/context/AuthContext';
+import { MenuLateral } from './src/navigator/MenuLateral';
 // import { Tabs } from './src/navigator/Tabs';
+import { AuthProvider } from './src/context/AuthContext';
+
 
 const App = () => {
   return (
@@ -18,11 +19,17 @@ const App = () => {
         {/* <Tabs /> */}
       </AppState>
     </NavigationContainer>
-  );
-};
+  )
+}
 
-const AppState = ({children}: any) => {
-  return <AuthProvider>{children}</AuthProvider>;
-};
+
+const AppState = ({ children }: any ) => {
+  return (
+    <AuthProvider>
+      { children }
+    </AuthProvider>
+  )
+}
+
 
 export default App;
